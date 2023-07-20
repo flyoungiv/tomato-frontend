0c8434544a02af827eb5f0500b4f3df3dfb4c5e9
Fleetwood <flyoungiv@gmail.com>
2023-07-20 09:46:30 -0500

    make call to fastapi and display in table

--- src/App.js
+++ src/App.js
@@ -20,32 +20,45 @@ const rows = [
 ];
 
 export default function App() {
+  const [data, setData] = React.useState([])
+
+  React.useEffect(() => {
+    console.log('start call')
+    fetch('http://localhost:8000/')
+      .then(res => res.json())
+      .then(body => setData(body))
+      .catch(e => console.log(e))
+  }, [])
+
   return (
     <div className="container">
       <TableContainer component={Paper}>
         <Table sx={{ minWidth: 650 }} aria-label="simple table">
           <TableHead>
             <TableRow>
-              <TableCell>Dessert (100g serving)</TableCell>
-              <TableCell align="right">Calories</TableCell>
-              <TableCell align="right">Fat&nbsp;(g)</TableCell>
-              <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-              <TableCell align="right">Protein&nbsp;(g)</TableCell>
+              <TableCell>Name</TableCell>
+              <TableCell align="right">Currency</TableCell>
+              <TableCell>Phone</TableCell> 
+              <TableCell>Email</TableCell>
+              <TableCell>Address</TableCell>
+              <TableCell>Zip</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
-            {rows.map((row) => (
+            {data.map((row, index) => (
               <TableRow
-                key={row.name}
+                key={index}
                 sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
               >
                 <TableCell component="th" scope="row">
                   {row.name}
                 </TableCell>
-                <TableCell align="right">{row.calories}</TableCell>
-                <TableCell align="right">{row.fat}</TableCell>
-                <TableCell align="right">{row.carbs}</TableCell>
-                <TableCell align="right">{row.protein}</TableCell>
+                <TableCell align="right">{row.currency}</TableCell>
+                <TableCell>{row.phone}</TableCell>
+                <TableCell>{row.email}</TableCell>
+                <TableCell>{row.address}</TableCell>
+                <TableCell>{row.postalZip}</TableCell>
+                
               </TableRow>
             ))}
           </TableBody>
